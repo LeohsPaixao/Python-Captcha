1b78ea5bb5c96cb87e0ea7f108ad62641c1c0cf8
Leonardo Paixão <leonardohspaixao@gmail.com>
2021-04-10 19:28:26 -0300

    reading the images and separating the letters of each image

--- separate_letters.py
+++ separate_letters.py
@@ -8,10 +8,10 @@ for file in files:
     image = cv2.imread(file)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     # Em Preto e Branco
-    _, image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV)
+    _, new_image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV)
 
     # Encontear os contornos de cada letra
-    contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(new_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     letters_region = []
 
@@ -29,3 +29,13 @@ for file in files:
     # desenhar os contornos e separar as letras em arquivos individuais
     final_image = cv2.merge([image] * 3)
 
+    i = 0
+    for rectangle in letters_region:
+        x, y, width, height = rectangle
+        letter_image = image[y-2:y+height+2, x-2:x+width+2]
+        i += 1
+        file_name = os.path.basename(file).replace(".png", f"letra{i}.png")
+        cv2.imwrite(f'letters/{file_name}', letter_image)
+        cv2.rectangle(final_image, (x-2, y-2),(x+width+2, y+height+2), (255, 0, 0), 1)
+    file_name = os.path.basename(file)
+    cv2.inwrite(f"letters_id/{file_name}", final_image)
